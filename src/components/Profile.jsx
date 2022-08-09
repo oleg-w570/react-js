765b2525c509dfc59ea9fd81adcbdf6ec1ec467e
oleg-w570 <ol3g.zor1n@yandex.ru>
2022-08-09 21:06:35 +0300

    1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img
+                src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'
+                alt='Something content'/>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
